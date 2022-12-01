994da67544eeca39d7b0272cab540c25e3ba36db
Giovanna <giovannagmoeller@gmail.com>
2022-12-01 15:49:09 -0300

    Aula 1.4 - Variáveis e Tipos de Dados

---
+++ Variaveis-Tipos-De-Dados.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var nome = "Giovanna"
+var idade = 22
+print(nome)
+print(idade)
+
+var coeficienteRendimento = 8.8
+var turnoEmQueEstuda: Character = "D"
+var possuiHistoricoReprovacao = false
+
+var idadeExplicita: Int = 22
+var nomeExplicito: String = "Giovanna"
+
+var variavelNaoInicializada: Int
+variavelNaoInicializada = 10
+
+let numeroConstante = 20
+// numeroConstante = 10
+
+typealias Estudante = String
+let estudante: Estudante = "Giovanna"
