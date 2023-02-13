586dc316e4e6f2098f137a435ffbd853547788c0
Giovanna <giovannagmoeller@gmail.com>
2023-02-13 18:43:58 -0300

    aula 1.4 - variáveis e tipos de dados

---
+++ Aula01.playground/Pages/Aula 1.4 - Variáveis e Tipos de Dados.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+var nome: String = "Giovanna"
+let idade: Int = 22
+
+print("O meu nome é \(nome)")
+print(idade)
+
+var coeficienteRendimento: Double = 9.5
+var possuiHistoricoReprovacao: Bool
+possuiHistoricoReprovacao = false
+
+var turnoEmQueEstuda: Character = "D"
+
+typealias Estudante = String
+
+var aluno1: Estudante = "João"
+var aluno2: Estudante = "Ana"
