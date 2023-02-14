194136ad38d02693ab0a4e04b5645c3f2809b477
Giovanna <giovannagmoeller@gmail.com>
2023-02-14 00:02:34 -0300

    aula 1.5 - operações aritméticas e de atribuição

---
+++ Aula01.playground/Pages/Aula 1.5 - Operações aritméticas e de atribuição.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+10 + 2
+10 - 2
+8 * 2
+10 / 2
+5.0 / 2
+// Double(5) / 2
+5 % 2
+4 % 2
+5 + 2 * 10
+(5 + 2) * 10
+
+var numero1 = 10
+var numero2 = 50
+var soma = numero1 + numero2
+print(soma)
+
+numero1 = numero1 + 2
+numero1 += 2
+numero1 -= 3
+numero1 *= 4
+numero1 /= 2
+
+var nome = "Giovanna"
+var sobrenome = "Moeller"
+print(nome + sobrenome)
+print("Meu nome é \(nome) \(sobrenome)")
