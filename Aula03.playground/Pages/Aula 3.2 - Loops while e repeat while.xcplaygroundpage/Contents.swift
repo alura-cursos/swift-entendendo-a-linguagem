1aea18b5bf3c1f5239620d2040a87a472d6350ba
Giovanna <giovannagmoeller@gmail.com>
2023-02-14 00:24:05 -0300

    aula 3.2 - loops while e repeat while

---
+++ Aula03.playground/Pages/Aula 3.2 - Loops while e repeat while.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+var count = 1
+
+while count <= 10 {
+    print(count)
+    count += 1
+}
+
+var count2 = 11
+
+repeat {
+    print(count2)
+    count2 += 1
+} while count2 <= 10
+            
+         
